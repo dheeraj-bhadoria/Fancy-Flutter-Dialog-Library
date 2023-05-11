f174fe0967439c7163f21381cec175d0ece40140
Dheeraj Singh Bhadoria <dheerubhadoria@gmail.com>
2023-05-11 12:03:50 +0530

    Initial release

---
+++ android/app/src/main/kotlin/com/example/fancy_flutter_dialog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fancy_flutter_dialog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/fancy_flutter_dialog.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FancyDialog extends StatelessWidget {
+
+  static show(){
+
+  }
+
+  static hide(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
